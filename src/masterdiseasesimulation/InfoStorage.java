d6eafb198452476e95012688ec3bd2a059a86a43
Alik15 <forregisteredsites@gmail.com>
2015-01-22 15:45:08 -0500

    First upload

---
+++ src/masterdiseasesimulation/InfoStorage.java
@@ -0,0 +1,37 @@
+package masterdiseasesimulation;
+
+public class InfoStorage {
+    private int day;
+    private int numSick;
+    private int cost;
+
+    public InfoStorage(int day, int numSick, int cost) {
+        this.day = day;
+        this.numSick = numSick;
+        this.cost = cost;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setNumSick(int numSick) {
+        this.numSick = numSick;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getNumSick() {
+        return numSick;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
---
+++ src/masterdiseasesimulation/PauseResume.java
@@ -0,0 +1,75 @@
+package masterdiseasesimulation;
+
+//Imports
+import java.awt.Frame;
+import javax.swing.JButton;
+
+public class PauseResume {
+	public PauseResume(Frame frame) {
+		counter.start();
+		button.addActionListener(pauseResume);
+
+		frame.add(button, java.awt.BorderLayout.NORTH);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	private JButton button = new JButton("Start");
+
+	private Object lock = new Object();
+	private volatile boolean paused = true;
+
+	private Thread counter = new Thread(new Runnable() {
+		@Override
+		public void run() {
+			while (true) {
+				work();
+			}
+		}
+	});
+
+	private void work() {
+		for (int i = 0; i < 10; i++) {
+			allowPause();
+			sleep();
+		}
+		done();
+	}
+
+	void allowPause() {
+		synchronized (lock) {
+			while (paused) {
+				try {
+					lock.wait();
+				} catch (InterruptedException e) {
+					// nothing
+				}
+			}
+		}
+	}
+
+	private java.awt.event.ActionListener pauseResume =
+			new java.awt.event.ActionListener() {
+		@Override
+		public void actionPerformed(java.awt.event.ActionEvent e) {
+			paused = !paused;
+			button.setText(paused ? "Resume" : "Pause");
+			synchronized (lock) {
+				lock.notifyAll();
+			}
+		}
+	};
+
+	private void sleep() {
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// nothing
+		}
+	}
+
+	private void done() {
+		button.setText("Pause");
+		paused = false;
+	}
+}
